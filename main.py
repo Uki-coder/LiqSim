fe56e84dbcced940c6431789f271cdbeeca3101c
AZAZALALKA <stusgivojno@gmail.com>
2024-12-18 13:59:05 +0100

    first commit
    same as prod branch

---
+++ main.py
@@ -0,0 +1,14 @@
+from plot_makers.surf_maker import surf_maker
+import numpy as np
+
+fun = lambda x, y, t: 25*np.sin(0.11*t*np.sqrt(x*x + y*y)) - np.cos(x*y)
+
+xlim = np.array([-50,50])
+ylim = np.array([-50,50])
+tlim = np.array([0,5])
+fps = 120
+N = 300
+zlim = np.array([-50, 50])
+
+mkr = surf_maker(N,fps,xlim,ylim,tlim,fun,zlim)
+mkr.save_animated_plot()
